0c97671cb04c8869735946f58ba3ae304f151f55
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-06-14 20:48:49 -0300

    Changing theme

--- SalesWebMvc/Migrations/20200612232920_Initial.cs
+++ SalesWebMvc/Migrations/20200612232920_Initial.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
+/*É usado aqui o workflow CODE-FIRST
+ * Existem basicamente 2 workflow`s para trabalhar com Migration
+ * o CODE-FIRST e o DATABASE-FIRST
+ * DATABASE-FIRST = o banco de dados é criado à partir do próprio banco de dados com comandos SQL, linguagem de BD relacional
+ * as classes são geradas à partir do BD na linguagem OO. Serviço dobrado.
+ * CODE-FIRST = o banco de dados é criado à partir das classes usando o Migration. Mais usado na Linguagem OO e mais prático.
+ */
+
 namespace SalesWebMvc.Migrations
 {
-    public partial class Initial : Migration
+    public partial class Initial : Migration // Migration: Script específico para gerar e versionar a base de dados
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
