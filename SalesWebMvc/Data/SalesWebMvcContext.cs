6ef38b44477e0a460196b8dcc3f46cc5c2850066
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-06-14 23:11:49 -0300

    Other entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
 
-namespace SalesWebMvc.Data
+
+namespace SalesWebMvc.Models // Substitui o using SalesWebMvc.Models;
 {
     public class SalesWebMvcContext : DbContext
     {
@@ -14,6 +14,8 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; } // Contexto para gravar as classes no DataBase
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
