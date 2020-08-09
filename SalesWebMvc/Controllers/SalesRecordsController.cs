170043e76b66d792f66e50aee018edf43614e226
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-08-08 22:36:01 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch() // Busca simples
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch() // Busca agrupada
+        {
+            return View();
+        }
+    }
+}
