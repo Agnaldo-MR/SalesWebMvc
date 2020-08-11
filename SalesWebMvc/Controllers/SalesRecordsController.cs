d33d4358f8fb76605fe95060ab12da4b007e4e15
Agnaldo MR <agnaldo.m.r@hotmail.com>
2020-08-11 12:22:35 -0300

    Implementing simple search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -3,19 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordService _salesRecordService;
+
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult SimpleSearch() // Busca simples
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate) // Busca simples
         {
-            return View();
+            if (!minDate.HasValue) // Negativa se possuir uma data (um valor) mínima(o)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+
+            if (!maxDate.HasValue) // Negativa se possuir uma data (um valor) mínima(o)
+            {
+                maxDate = DateTime.Now;
+            }
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+
+            return View(result);
         }
 
         public IActionResult GroupingSearch() // Busca agrupada
---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SalesWebMvcContext _context; // "readonly" = para prevenir que esta dependência não possa ser alterada
+
+        public SalesRecordService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate) // Datas opcionais
+        {
+            var result = from obj in _context.SalesRecord select obj;
+
+            if (minDate.HasValue) // Se foi informada uma data mínima faça
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue) // Se foi informada uma data máxima faça
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller) // Para fazer o join das tabelas
+                .Include(x => x.Seller.Department) // Para faer o join com departamentos
+                .OrderByDescending(x => x.Date) // Ordenar por data
+                .ToListAsync(); // Retornar uma lista assíncrona
+        }
+    }
+}
